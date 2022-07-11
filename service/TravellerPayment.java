1bc5299fa9f6c901e3ce571cc556df0bdf0d5d27
mrameshece2003 <106156610+mrameshece2003@users.noreply.github.com>
2022-07-11 15:33:29 +0530

    Add files via upload

---
+++ service/TravellerPayment.java
@@ -0,0 +1,42 @@
+package travel.service;
+
+import java.util.Scanner;
+
+public class TravellerPayment {
+	public void getDenominations(int denominations[]) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the currency denominations value ");
+		for(int index=0; index < denominations.length; index++) {
+			denominations[index] = scan.nextInt();
+		
+		}
+	
+	}
+
+
+	public void denominationsToPay(int denominations[], int amountToPay) {	
+		int denominationsToPay[] = new int[denominations.length];
+
+		int amount = amountToPay;
+
+		for(int index =0; index < denominations.length; index++) {
+			denominationsToPay[index] = amount / denominations[index];
+			amount -= (denominations[index] * denominationsToPay[index]);
+		}
+		if(amount != 0) {
+			//System.out.println("Sorry can’t make exact payment");
+			//return;
+	denominationsToPay[denominations.length-1]++;
+		}
+
+		System.out.println("To pay amountToPay Pay in ");
+		for(int index=0; index < denominations.length; index++) {
+			if(denominationsToPay[index] != 0) {
+				System.out.println(denominations[index] + " : " + denominationsToPay[index]);
+			}
+		}
+
+	}
+}
+
